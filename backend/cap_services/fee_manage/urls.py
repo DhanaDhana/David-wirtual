c400c3ada9a3434fb4680b6c5bda96da1da547b4
bibin-wirtuallife <bibin.j@wirtual.life>
2022-02-25 12:41:41 +0530

    Update urls.py

--- backend/cap_services/fee_manage/urls.py
+++ backend/cap_services/fee_manage/urls.py
@@ -13,9 +13,9 @@ router.register('clientlist-monthly-issue',views.clientDetailsInMonthlyIssue, ba
 urlpatterns = [
     path('home/',views.home),
     path('getUser',views.getUsers.as_view()),
-    path('advisor/monthly-issue/',views.searchAdvisorsForMonthlyIssue.as_view()),
-    path('search-advisor-pending-issue/',views.searchAdvisorPendingIssue.as_view()),
-    path('advisor-list-pending-issue/',views.advisorDetailsInPending.as_view()),
+    path('advisor/monthly-issue/',views.searchAdvisorsForMonthlyIssue.as_view(),name = 'advisormonthlyissue'),
+    path('search-advisor-pending-issue/',views.searchAdvisorPendingIssue.as_view(),name = 'advisorPendingIssue'),
+    path('advisor-list-pending-issue/',views.advisorDetailsInPending.as_view(),name = 'advisorListPending'),
 ]
 
 
